4dfec6fff0b6a6408948068565291715d494d35b
sunzhen <sunzhen@shlanbao.cn>
2019-06-06 10:02:06 +0800

    添加了一个add方法

--- Test/src/com/sunzhen/test/demo01.java
+++ Test/src/com/sunzhen/test/demo01.java
@@ -4,4 +4,9 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("!!!!!!!!!");
     }
+
+    public void add(){
+        int i=1+1;
+        System.out.println(i);
+    }
 }
